61fa23eb0d625e858145d93f454fd3ec3a91a89c
tonycar12002 <tonycar12002@gmail.com>
2018-06-18 10:47:53 +0800

    finish week01

--- Data_Structures/week01/HW/03_Network_packet_processing_simulation.cpp
+++ Data_Structures/week01/HW/03_Network_packet_processing_simulation.cpp
@@ -14,15 +14,49 @@ struct Request
 
 struct Response
 {
+	Response(bool dropout, int finish_time, int start_time):
+		dropout(dropout), finish_time(finish_time), start_time(start_time)
+	{}
 	bool dropout;
 	int finish_time;
+	int start_time;
 };
 
-void processRequests(vector<Request> requests){
+void processRequests(vector<Request> requests, int buffer_size){
 	queue<Response> buffer;
+	vector<Response> responses;
 	int now_time = 0;
 	for(int i=0;i<requests.size();i++){
+		if(buffer.size()<buffer_size){		
+			if(now_time<=requests[i].arrive_time)
+				now_time = requests[i].arrive_time + requests[i].process_time;
+			else
+				now_time += requests[i].process_time;	
 
+			responses.push_back(Response(false, now_time, now_time-requests[i].process_time));
+			buffer.push(Response(false, now_time, now_time-requests[i].process_time));
+		}
+		else if(buffer.front().finish_time <= requests[i].arrive_time){
+			if(now_time<=requests[i].arrive_time)
+				now_time = requests[i].arrive_time + requests[i].process_time;
+			else
+				now_time += requests[i].process_time;
+
+			responses.push_back(Response(false, now_time, now_time-requests[i].process_time));
+			buffer.pop();
+			buffer.push(Response(false, now_time, now_time-requests[i].process_time));
+		}
+		else if(buffer.front().finish_time > requests[i].arrive_time){
+			responses.push_back(Response(true, -1, -1));
+		}
+		else;
+	}
+
+	for(int i=0;i<responses.size();i++){
+		if(responses[i].dropout)
+			cout << -1 << endl;
+		else
+			cout << responses[i].start_time << endl;
 	}
 }
 
@@ -37,10 +71,9 @@ int main(int argc, char **argv){
 		cin >> arrive_time >> process_time;
 		requests.push_back(Request(arrive_time, process_time));
 	}
-	processRequests(requests);
 	if(n==0);
 	else{
-
+		processRequests(requests, s);
 	}
 	return 0;
 }
