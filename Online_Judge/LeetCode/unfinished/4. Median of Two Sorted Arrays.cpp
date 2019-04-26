c169f9ccee426c09124f28bdcdcb1ddd6e8807d0
Tony Hsiao <tonycar12002@gmail.com>
2019-04-26 11:09:48 +0800

    update

--- Online_Judge/LeetCode/unfinished/4. Median of Two Sorted Arrays.cpp
+++ Online_Judge/LeetCode/unfinished/4. Median of Two Sorted Arrays.cpp
@@ -8,7 +8,15 @@ Not yet
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        
+        int len = nums1.size() + nums2.size();
+        int mid = len/2;
+        int left_x = 0, right_x = nums1.size()-1, left_y = 0, right_y = nums2.size()-1;
+        bool is_odd = (len%2 == 1) ? true : false;
+
+        while(left_x < right_x || left_y < right_y){
+            int mid_x = (left_x + right_x)/2;
+            int mid_y = (left_y + right_y)/2;
+        }
         
     }
 };
