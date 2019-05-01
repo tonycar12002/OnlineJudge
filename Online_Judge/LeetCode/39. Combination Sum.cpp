7a06c1ce5531dd329109875d51b2ac2e71033d43
Tony Hsiao <tonycar12002@gmail.com>
2019-05-02 01:04:20 +0800

    update

---
+++ Online_Judge/LeetCode/39. Combination Sum.cpp
@@ -0,0 +1,30 @@
+/*
+Author: Tony Hsiao
+Date: 2019/05/01
+Topic: 39. Combination Sum
+Note: DFS
+*/
+class Solution {
+private:
+    vector<vector<int>> ans;
+public:
+    void FindTarget(vector<int>& candidates, vector<int> cur_solution, int target, int index){
+        if (target == 0)
+            ans.push_back(cur_solution);
+        for(int i = index ; i < candidates.size() ; i++){
+            if(candidates[i] > target)
+                break;
+            cur_solution.push_back(candidates[i]);
+            FindTarget(candidates, cur_solution, target - candidates[i], i);
+            cur_solution.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+
+        vector<int> cur;
+        FindTarget(candidates, cur, target, 0);
+
+        return ans;
+    }
+};
